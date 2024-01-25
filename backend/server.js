8b671e5e77812abb48d4e57d23c7431e93e4eaf9
FrederickNN <fnoronhanehr@student.unimelb.edu.au>
2024-01-25 16:36:22 +1100

    fixing cors issue 2

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,18 @@ if (process.env.NODE_ENV !== 'production'){
 
 // Enable CORS for development
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ftfiiilsbvlrqeolrdefqxii.vercel.app/'|| 'http://localhost:3000');
+  const allowedOrigins = ['https://ftfiiilsbvlrqeolrdefqxii.vercel.app/', 'http://localhost:3000'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
 
 // Parse JSON requests
 app.use(express.json());
